32585bb5611b5671ac286370569de7d43785f5bf
ConspiracyTheory13 <33845593+ConspiracyTheory13@users.noreply.github.com>
2019-05-19 16:41:18 -0700

    Add files via upload

---
+++ questionLibrary.js
@@ -0,0 +1,105 @@
+export const STORE = [
+     {
+          question: 'You value your status as an informed person who always watches the news and stays up to date on local and world politics via the internet and social media. However, you are often afraid and imagine that crime rates are out of control. Your mind is using the:',
+          answers: [
+               'Depressive Realism heuristic.',
+               'Availability heuristic.',
+               'Representativeness heuristic.',
+               'Anchoring and Adjustment heuristic'
+          ],
+          correctAnswer: 'Availability heuristic.'
+          // ,icon: somethingcooloragif.png,
+          // alt: 'idk'
+     },
+     {
+          question: 'Certain words and phrases have powerful emotional affects, when your stomache clenches in dread at the word “cancer” or you feel a sense of unease from the word “death”. What shortcut is your mind using?',
+          answers: [
+               'Affect heuristic.',
+               'Terror Management Theory',
+               'Scapegoat Theory',
+               'Gut Reaction heuristic'
+          ],
+          correctAnswer: 'Affect heuristic.'
+     },
+     {
+          question: 'You and your buddy Larry are flipping coins for a probability experiment. Somehow you’ve come up with heads five times in a row! “That’s it,” Larry says. “The next one has got to be tails. It’s statistically impossible!”. Larry is using the:',
+          answers: [
+               'Law of the instrument effect',
+               'Statistical Bias',
+               'Gambler Fallacy',
+               '50/50 Theory'
+          ],
+          correctAnswer: 'Gambler Fallacy'
+     },
+     {
+          question: 'The speaker at the cat flatterer convention is asking for audience members to guess the value of an ultra deluxe set of premium cat furniture. “How much do you think it’s worth?” he asks the audience. “At least 3k, right?” the tendency for the audience to then venture guesses closer in range to the speakers represents the:',
+          answers: [
+               'Attentional Bias',
+               'Fluency Heuristic',
+               'Anchoring Heuristic',
+               'Speaker Fallacy'
+          ],
+          correctAnswer: 'Anchoring Heuristic',
+     },
+     {
+          question: 'The tendency for people to value that which is communicated more skillfully or elegantly represents the:',
+          answers: [
+               'Anchoring Heuristic',
+               'Fluency Heuristic',
+               'Brain in a Vat',
+               'Same Bias'
+          ],
+          correctAnswer: 'Fluency Heuristic',
+     },
+     {
+          question: 'The tendency to ignore (generic, general information) and focus on specific information is the:',
+          answers: [
+               'Simulation heuristic.',
+               'Gambler’s Fallacy',
+               'Impact Bias',
+               'Base rate fallacy'
+          ],
+          correctAnswer: 'Base rate fallacy',
+     },
+     {
+          question: 'You watch with baited breath as your race horse glides neck and neck with another, round and around the track. On the final lap your house begins to lag, but at the stretch makes a miraculous recovery. Nonetheless, your horse still loses. “It would have been better if it hadn’t been close at all!” you lament, racking your brain over all the small differences that would have let your horse win. This is an example of the:',
+          answers: [
+               'Simulation heuristic',
+               'Gambler’s Fallacy',
+               'Impact Bias',
+               'Learned Helplessness'
+          ],
+          correctAnswer: 'Simulation heuristic',
+     },
+     {
+          question: 'The tendency for a person to recognize and remember details that confirm preconceived notions is an example of:',
+          answers: [
+               'Simulation heuristic.',
+               'Observer-expectancy Effect',
+               'Optimism Bias',
+               'Confirmation bias'
+          ],
+          correctAnswer: 'Confirmation bias',
+     },
+     {
+          question: 'The tendency to prescribe a person’s apparent behavior to their personality and to ignore circumstance is the:',
+          answers: [
+               'Simulation heuristic.',
+               'Gambler Fallacy',
+               'Fundamental Attribution Error',
+               'Learned Helplessness',
+
+          ],
+          correctAnswer: 'Fundamental Attribution Error',
+     },
+     {
+          question: 'The tendency for an individual to attribute the overall validity of a decision based on the eventual outcome is known as',
+          answers: [
+               'Outcome Bias',
+               'Normalcy Bias',
+               'Omission Bias',
+               'Learned Helplessness'
+          ],
+          correctAnswer: 'Outcome Bias',
+     }
+];
