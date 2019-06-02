f5f8e0148aed850b326154bfa7e6e3058c216e06
Charla Bigelow <33845593+ConspiracyTheory13@users.noreply.github.com>
2019-06-01 17:33:14 -0700

    Add files via upload

--- index2.js
+++ index2.js
@@ -1,43 +1,121 @@
-import {STORE} from './questionLibrary.js';
+
+// import {STORE} from './questionLibrary2.js';
+// function questionLibrary() {
+    const STORE = [{
+             question: 'You value your status as an informed person who always watches the news and stays up to date on local and world politics via the internet and social media. However, you are often afraid and imagine that crime rates are out of control. Your mind is using the:',
+             answers: {
+                a:'Depressive Realism heuristic.',
+                b:'Availability heuristic.',
+                c:'Representativeness heuristic.',
+                d:'Anchoring and Adjustment heuristic'
+             },
+             correctAnswer: 'b'
+             },
+        
+             {
+             question: 'Certain words and phrases have powerful emotional affects, when your stomache clenches in dread at the word “cancer” or you feel a sense of unease from the word “death”. What shortcut is your mind using?',
+             answers: {
+                a:'Affect heuristic.',
+                b:'Terror Management Theory',
+                c:'Scapegoat Theory',
+                d:'Gut Reaction heuristic'
+        },
+             correctAnswer: 'a'
+//this is where the rest of the questions will be pasted in.
+            }
+    ];
+
 
 let incorrectScore = 0;
 let correctScore = 0;
 let questionNumber = 0;
 let currentQuestion = 0;
+let letter = [];
+let currentSlide = 0;
 
-// const submitBttn = `<button type="submit" id="submitBttn" class= "bttn" hidden>Score Me!</button>`;
-// const replayBttn = `<button type="replay" id="replayBttn" class= "bttn" hidden>Play again?</button>`;
-// const nextBttn = `<button type="next" id="nextBttn" class="bttn">Next Question!</button>`;
+let buttons =
+`<button type="button" class="nextButton hidden" id="nextButton">Next Question!</button>       
+<button type="button" class="bttn hidden" id="scoreButton">Score me!</button>       
+<button type="button" class="bttn hidden" id="replayButton">Score me!</button>`;  
 
-let questionForm =
-    `<div class= "quizProgressionTracker">
-    <ul><li>Question: <span class="currentQuestion"> ${currentQuestion} </span>/10</li> </ul>
-    <li> Incorrect Score: ${incorrectScore}</li>
-    <li> Correct Score: ${correctScore}</li>
-    </div> 
+function buildQuizForm() {
+    console.log('build quiz form is being run!')
+     var output = [];
+ STORE.forEach((currentQuestion, questionNumber) => {
+        const answers = [];
+        for (var letter in currentQuestion.answers) {
+            answers.push( `<label>
+          <input type ="radio" name="question${questionNumber}" value = "${letter}">
+          ${letter}:
+          ${currentQuestion.answers[letter]}
+          </label> <br>`);
+        }
+        //most likely problem is that you're using this id="slideBackground"
+        //`id=${'slideBackground'+questionNumber}
+        
+        output.push(
+          `<div class="slideBackground">
+                <div class="slide" +  id="currentSlide[` + questionNumber.toString() + `]">    
+                    <div class="table" id="quizProgressionTracker">
+                        <ul id="horizontal-list">
+                            <li>Question: <span class="currentQuestion">` + $(currentQuestion) + `</span>/10</li>
+                            <li> Incorrect Score: ${incorrectScore}</li>
+                            <li> Correct Score: ${correctScore}</li>
+                        </ul>
+                    </div>
+                    <div class="question-${questionNumber}">
+                    <h1>${STORE[questionNumber].question}</h1>
+                    <div class="answers">${answers.join("")}</div>
+                    <button type="button" class="bttn hidden" id="nextButton">Next Question!</button>
+                </div>
+                </div>`
+)
+    quizModalContainer.innerHTML = output.join("")
+})
+}
 
-    <div class="question-${questionNumber}">
-    <h1>${STORE[questionNumber].question}</h1>
-    <form class="answerContainer">
-    <fieldset>
-    <label class="answerOption">
-    <input type="radio" value="${STORE[questionNumber].answers[0]}" name="answer" required>
-    <span>${STORE[questionNumber].answers[0]}</span>
-    </label>
-    <label class="answerOption">
-    <input type="radio" value="${STORE[questionNumber].answers[1]}" name="answer" required>
-    <span>${STORE[questionNumber].answers[1]}</span>
-    </label>
-    <label class="answerOption">
-    <input type="radio" value="${STORE[questionNumber].answers[2]}" name="answer" required>
-    <span>${STORE[questionNumber].answers[2]}</span>
-    </label>
-    <label class="answerOption">
-    <input type="radio" value="${STORE[questionNumber].answers[3]}" name="answer" required>
-    <span>${STORE[questionNumber].answers[3]}</span>
-    </label>
-    </div>
-    <button type="nextQuestion" id="nextBttn" class="bttn">Next Question</button>;`  
+const nextButton = document.getElementById("nextButton");
+const slides = document.querySelectorAll(".slide");
+
+//function called Create Slide
+
+//so for my pagination I have set all slides to having a class = "slide hidden". 
+//so if I need to use my function createSlide to target 'slide' + [questionNumber] and remove class "hidden" 
+
+
+// currentSlide = document.getElementById('currentSlide' + [questionNumber])
+
+// const createSlide = () => {
+//     console.log("createslide is firing");
+//     let currentSlide = document.getElementById('currentSlide['+ questionNumber.toString() + ']');
+//     // currentSlide.classList.toggle("hidden", currentQuestion <= 10 );
+
+// };
+
+//   const showNextSlide = () => {
+//     updateQuestionNumber();
+//     updateCurrentQuestion();             
+//     showSlide(currentSlide + 1);
+//   }
+
+
+
+  const startQuiz = () => {
+    console.log('startQuiz is being run!')
+     $("#quizModalContainer").html("#modalGutsTemplate")
+     $("#quizProgressionTracker").html("#quizProgressionTracker")
+     $("#questionsAndAnswers").html(buildQuizForm())
+     $("#buttonContainer").html(buttons)
+    //  createSlide();
+    //  $("#nextButton").removeClass("hidden");
+    //  hideStartPage();
+     };
+
+
+const hideStartPage = () => { 
+    console.log('hidestartpage running');
+     $("#startingQuizPage").addClass("hidden");
+}
 
 const incremementCorrectScore = () => {
     correctScore++;
@@ -55,7 +133,7 @@ const updateCurrentQuestion = () => {
 const updateQuestionNumber = () => { 
     questionNumber ++;
     $('.questionNumber').text(questionNumber+1)
-}
+};
 
 const userCorrectAnswer = () => {
     incremementCorrectScore();
@@ -66,48 +144,18 @@ const userIncorrectAnswer = () => {
     incrementIncorrectScore();
     incorrectAnswerFeedback();
 }
-
-const startQuiz = () => { 
-    $("#quizModalContainer").modal({
-              fadeDuration: 100,
-              closeExisting: false,
-              escapeClose: false,
-              clickClose: false,
-              showClose: false
-    }).append($(questionForm));
-};
-
-const renderQuestion = () => {
-    $('#quizModalContainer').html(questionForm());
-};
   
-const renderNextQuestion = () => {
-        $('#quizModalContainer').on('click', '#nextBttn', function (event) {
-        scoreUserInput();
-        renderQuestion();
-        updateCurrentQuestion();
-        updateQuestionNumber();
-  })};
-
-// let scoreUser = (correctScore) => {
-//     if (correctScore === 10) {
-//         $('#quizModalContainer').html( `div class="perfectScore">"perfectScore!"</div>`)  
-//     } else if (correctScore < 9 && correctScore >= 5) {
-//         $('#quizModalContainer').html( `div class="goodScore">"goodScore!"</div>`) 
-//     } else {
-//         $('#quizModalContainer').html( `div class="badScore">"Please try again!"</div>`)
-//     }
-    
-//     return (correctScore === 10) ? $('#quizModalContainer').html
-//          : (correctScore < 9 && correctScore >=5) ? $('#quizModalContainer').html( `div class="goodScore">"goodScore!"</div>`) 
-//          : (correctScore < 5) ? $('#quizModalContainer').html( `div class="badScore">"Please try again!"</div>`)
-// };
-
 let replayQuiz = () => {
     location.reload();
 };
 
-    document.getElementById('startBttn').addEventListener("click", startQuiz);
-    document.getElementById('nextBttn').addEventListener("click", renderNextQuestion);
-    document.getElementById('replayBttn').addEventListener("click", replayQuiz);
-    document.querySelector('form').addEventListener("submit", scoreUserInput);
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById('startButton').addEventListener("click", startQuiz);
+    // document.getElementById('nextButton').addEventListener("click", plusSlides(n));
+    // document.getElementById('replayButton').addEventListener("click", replayQuiz);
+    // document.querySelector('form').addEventListener("submit", scoreUserInput);
+
+
+  
+});  
+  
